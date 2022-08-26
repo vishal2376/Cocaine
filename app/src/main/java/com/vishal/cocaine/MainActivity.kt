5e1eed37cc30879ec0c6efef53117ce9d37d2da4
vishal2376 <vishalsingh2376@gmail.com>
2022-08-26 15:31:10 +0530

    bottom nav click working

--- app/src/main/java/com/vishal/cocaine/MainActivity.kt
+++ app/src/main/java/com/vishal/cocaine/MainActivity.kt
@@ -2,10 +2,39 @@ package com.vishal.cocaine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.vishal.cocaine.fragments.FavoriteFragment
+import com.vishal.cocaine.fragments.HomeFragment
+import com.vishal.cocaine.fragments.MusicFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val musicFragment = MusicFragment()
+    private val favoriteFragment = FavoriteFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.itemHome -> loadFragment(homeFragment)
+                R.id.itemMusic -> loadFragment(musicFragment)
+                R.id.itemFavorite -> loadFragment(favoriteFragment)
+            }
+            true
+        }
+
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.mainContainer,fragment)
+        transaction.commit()
     }
 }
