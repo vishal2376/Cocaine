0de80da2a3068951b7a26c0c5ea83dd2f7487eac
vishal2376 <vishalsingh2376@gmail.com>
2022-09-05 11:51:43 +0530

    now playing layout set working

--- app/src/main/java/com/vishal/cocaine/fragments/NowPlayingFragment.kt
+++ app/src/main/java/com/vishal/cocaine/fragments/NowPlayingFragment.kt
@@ -1,18 +1,65 @@
 package com.vishal.cocaine.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vishal.cocaine.PlayerActivity
 import com.vishal.cocaine.R
+import com.vishal.cocaine.databinding.FragmentNowPlayingBinding
+import com.vishal.cocaine.models.setImgArt
+import kotlinx.android.synthetic.main.fragment_now_playing.*
 
 class NowPlayingFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_now_playing, container, false)
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var binding: FragmentNowPlayingBinding
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_now_playing, container, false)
+        binding = FragmentNowPlayingBinding.bind(view)
+        binding.root.visibility = View.INVISIBLE
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (PlayerActivity.musicService != null) {
+            binding.root.visibility = View.VISIBLE
+
+            setLayoutNP()
+        }
+
+    }
+
+    // set layout of Now Playing
+    private fun setLayoutNP() {
+
+        //song image
+        setImgArt(
+            requireContext(), PlayerActivity.songListPA[PlayerActivity.songPosition].path,
+            binding.imgCurrentSongNP
+        )
+
+        //song title
+        binding.tvSongTitleNP.text =
+            PlayerActivity.songListPA[PlayerActivity.songPosition].title
+
+        //moving title
+        tvSongTitleNP.isSelected = true
+
+        //set play pause icon
+        if (PlayerActivity.isPlaying)
+            binding.imgPlayPauseNP.setImageResource(R.drawable.ic_pause)
+        else
+            binding.imgPlayPauseNP.setImageResource(R.drawable.ic_play)
+
+    }
 }
