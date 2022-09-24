ac1f5740b4a85b06c1efa137b32c3200bdb35daa
vishal2376 <vishalsingh2376@gmail.com>
2022-09-24 22:44:57 +0530

    alert box implemented

--- app/src/main/java/com/vishal/cocaine/fragments/PlaylistFragment.kt
+++ app/src/main/java/com/vishal/cocaine/fragments/PlaylistFragment.kt
@@ -1,20 +1,26 @@
 package com.vishal.cocaine.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.vishal.cocaine.R
 import com.vishal.cocaine.adapters.PlaylistAdapter
+import com.vishal.cocaine.databinding.FragmentPlaylistBinding
 import com.vishal.cocaine.models.Playlist
 import kotlinx.android.synthetic.main.fragment_playlist.*
 
 class PlaylistFragment : Fragment() {
 
-    companion object{
-       var playlistPF:ArrayList<Playlist> = ArrayList()
+    companion object {
+        var playlistPF: ArrayList<Playlist> = ArrayList()
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var binding: FragmentPlaylistBinding
     }
 
     override fun onCreateView(
@@ -22,23 +28,44 @@ class PlaylistFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_playlist, container, false)
+        FragmentPlaylistBinding.bind(view)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        //add playlist button
+        addPlaylistDialog()
 
         //set recycler view
         setRecyclerPF()
 
+        return view
+    }
+
+    private fun addPlaylistDialog() {
+        val addDialog =
+            LayoutInflater.from(requireContext()).inflate(R.layout.playlist_dialog, binding.root,false)
+        val builder = MaterialAlertDialogBuilder(requireContext())
+        builder.setTitle("New Playlist")
+        builder.setPositiveButton("ADD"){dialog,_ ->
+            dialog.dismiss()
+        }
     }
 
     private fun setRecyclerPF() {
         //temp list
-        val tempData:Array<String> = arrayOf("Playlist 0","Playlist 1","Playlist 2","Playlist 3","Playlist 1","Playlist 2","Playlist 3","Playlist 4","Playlist 5",)
+        val tempData: Array<String> = arrayOf(
+            "Playlist 0",
+            "Playlist 1",
+            "Playlist 2",
+            "Playlist 3",
+            "Playlist 1",
+            "Playlist 2",
+            "Playlist 3",
+            "Playlist 4",
+            "Playlist 5",
+        )
 
-        rvPlaylistPF.layoutManager = GridLayoutManager(requireContext(),2)
-        rvPlaylistPF.adapter = PlaylistAdapter(requireContext(),tempData)
+        rvPlaylistPF.layoutManager = GridLayoutManager(requireContext(), 2)
+        rvPlaylistPF.adapter = PlaylistAdapter(requireContext(), tempData)
     }
 
 }
